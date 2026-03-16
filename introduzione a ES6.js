c161693cb2fdcf1df23f39e82f13ceeb683ceba3
Gianni <BussolettiGianni@gmail.com>
2026-03-16 11:59:35 +0100

    Update introduzione a ES6.js

--- introduzione a ES6.js
+++ introduzione a ES6.js
@@ -156,3 +156,95 @@ const cantautore = {
 const { firstName, lastName, age } = cantautore
 // questa scorciatoia crea direttamente delle variabili con il nome delle chiavi
 // all'interno dell'oggetto andandole ad estrarre in una nuova variabile
+
+// parliamo di stringhe ...
+// in JS ci sono 2 delimitatori di stringhe:
+const str1 = "ciaone!"
+
+const str2 =
+  "Si utilizzano entrambi quando in una stringa dovete inserire un apostrofo ... l'albero"
+
+//però in realtà c'è un TERZO MODO: i BACKTICKS
+
+const myFirstName = "Stefano"
+const myLastName = "Casasola"
+
+const message = "Il mio nome è " + myFirstName + " e il mio cognome è " + myLastName
+// 'Il mio nome è Stefano e il mio cognome è Casasola'
+
+const betterMessage = `Il mio nome è ${myFirstName}
+e il mio cognome è ${myLastName}`
+
+// I BACKTICKS `` PERMETTONO DI SPEZZARE UNA LINEA DI STRINGA ANDANDO ACCAPO
+// il simbolo del dollaro $ permette invece ci richiamare la variabile in una stringa
+// senza dover usare virgole o più +. e mi permette inoltre di usare virgolette "" e
+// accenti '' come caratteri di string e non come apertura e chiusura di una frase
+
+const aVeryLongText = `Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+Nostrum praesentium deserunt consequatur ex ipsa, natus minus iste voluptatibus
++numquam, soluta asperiores vitae harum possimus expedita qui enim est quidem saepe.
+Lorem ipsum dolor sit amet ${myLastName} consectetur adipisicing elit. Deserunt iste sunt fuga 
+nisi odio. Quas animi necessitatibus expedita possimus, id maxime placeat ratione 
+provident, corrupti enim laboriosam. Eum, eligendi sint. Lorem ipsum dolor sit amet, 
+consectetur adipisicing elit. Molestiae ${myFirstName} laudantium repellendus voluptatibus minus
+optio quaerat magnam rerum eaque corrupti reprehenderit`
+
+// METODI DELLE STRINGHE
+
+const unaStringa = "il JAVASCRIPT, è un bel linguaggio"
+
+// come per gli array anche le stringe hanno una index che parte da 0
+unaStringa.toUpperCase() // "IL JAVASCRIPT  È UN BEL LINGUAGGIO"
+unaStringa.toLowerCase() // "il javascript è un bel linguaggio"
+unaStringa.slice(0, 10) // prende i primi 10 caratteri della frase le proprietà all'interno delle parentesi
+// sono il punto di partenza e il numero di caratteri da prendere
+unaStringa.charAt(2) // prende il singolo carattere nella frase --> "I" tramite la sua index
+unaStringa.split(" " /* "i" ","*/) // trasforma la stringa in un array usando i simboli dati
+// come proprietà, in questo caso lo spazio, ma potrebbe essere
+// anche una lettera o un simbolo speciale
+
+// startsWith(), endsWith(), includes() sono metodi delle stringe
+// che ritornano una valore booleano
+
+const course = "EPICODE"
+course.startsWith("EP") // true --> parte dall'inizio della stringa e controlla se ci sono tutti i caratteri
+course.endsWith("DE") // true --> parte dalla fine della stringa e controlla se ci sono tutti i caratteri
+course.startsWith("PICO") // false
+console.includes("CO") // true --> controlla se il valore dato si trova all'interno della stringa
+
+course.indexOf("E") // 0 --> è il contrario di .charAt  dando la posizione index
+// in cui compare per la prima volta il carattere richiesto
+
+// METODI DEGLI ARRAY
+// Un array è una struttura dati complessa in JS che memorizza diversi elementi all'interno di una singola
+// variabile; gli elementi (a differenza degli oggetti) NON sono "etichettati" con una chiave, ma il fattore
+// che li differenzia è solamente la loro POSIZIONE all'interno dell'array: c'è il PRIMO elemento dell'array
+// il SECONDO, il TERZO e così via. La loro posizione (detta INDICE o INDEX in inglese) parte da ZERO.
+
+const myArrayOfBeautifulPets = ["tarantula", "salamander", "alligator", "furby"]
+myArrayOfBeautifulPets.push("ralph") // aggiunge in coda all'array
+myArray0fBeautifulPets.pop() // elimina l'ultimo elemento dell'array
+myArrayOfBeautifulPets.unshift("cobra") // aggiunge in cima all'array
+myArrayOfBeautifulPets.shift() // rimuove il primo elemento dell'array
+
+// per aggiungere o rimuovere in mezzo all'array si utilizza SPLICE()
+// per inserire un altro elemennto tra salamandra ed alligatore
+
+myArrayOfBeautifulPets.splice(2, 0, "anaconda") // dalla posizione DUE dell'array, tolgo ZERO elementi, aggiungo "anaconda"
+myArrayOfBeautifulPets.splice(2)
+// il primo elemento di un array ha sempre indice 0
+// l'ultimo esemento ha sempre lenght-1
+
+// ci siamo resi conto scorsa settimana che il ciclo FOR è uno strumento PERFETTO per ESPLORARE gli ARRAY!
+// se noi facciamo un FOR con una i che comincia da 0 e finisce PRIMA di raggiungere la length
+// di una array, otteniamo SEMPRE una i che è un INDICE VALIDO per quell'array!
+
+for (let i = 0; i < myArrayOfBeautifulPets.length; i++) {
+  // la i varrà nelle varie iterazioni del for: 0 - 1 - 2 - 3
+  // quindi è sempre un NUMERO, ma rappresenta anche un indice valido per un elemento di myArrayOfBeautifulPet
+  console.log(i) // 0 - 1 - 2 - 3
+  console.log(myArrayOfBeautifulPets[i]) // 'tarantula' - 'salamander' - 'alligator' - 'furby'
+}
+
+// NUOVI METODI DEGLI ARRAY, INTRODOTTI CON ES6
+// forEach(), map(), filter(), reduce()
